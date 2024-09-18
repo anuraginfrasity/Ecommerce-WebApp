6b0d492df25e4d407fdfdd42586076e2076eccce
anuraginfrasity <anurag@infrasity.com>
2024-09-18 16:30:35 +0530

    Update views.py

--- ecom/views.py
+++ ecom/views.py
@@ -531,8 +531,9 @@ def edit_profile_view(request):
     userForm=forms.CustomerUserForm(instance=user)
     mydict={'userForm':userForm,'customerForm':customerForm}
     if request.method=='POST':
-        userForm=forms.CustomerUserForm(request.POST,instance=user)
+        
         customerForm=forms.CustomerForm(request.POST,instance=customer)
+        userForm=forms.CustomerUserForm(request.POST,instance=user)
         if userForm.is_valid() and customerForm.is_valid():
             user=userForm.save()
             user.set_password(user.password)
